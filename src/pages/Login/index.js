76e727a4abc8d6589f793fb1da20b96243462359
Fabiano Raiser <fabiano.raiser16@gmail.com>
2023-06-17 09:14:33 -0300

    :recycle: refactor: Migrando os States do Usuário para UsuarioContext

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -7,50 +7,51 @@ import {
 import {
   Input,
   InputLabel,
-  InputAdornment 
+  InputAdornment
 } from '@material-ui/core';
 import { useHistory } from 'react-router-dom';
+import { UsuarioContext } from 'common/Context/Usuario';
 
-function Login({ nome, setNome, saldo, setSaldo }) {
+function Login() {
   const history = useHistory();
 
   return (
     <Container>
-      <Titulo>
-        Insira o seu nome
-      </Titulo>
-      <InputContainer>
-        <InputLabel>
-          Nome
-        </InputLabel>
-        <Input
-          value={nome}
-          onChange={(event) => setNome(event.target.value)}
-          type="text"
-        />
-      </InputContainer>
-      <InputContainer>
-        <InputLabel>
-          Saldo
-        </InputLabel>
-        <Input
-        type="number"
-        value={saldo}
-        onChange={(event) => setSaldo(event.target.value)}
-        startAdornment={
-          <InputAdornment position="start">
-            R$
-          </InputAdornment>
+      <UsuarioContext.Consumer>
+        {({ nome, setNome, saldo, setSaldo}) => (
+          <>
+            <Titulo>
+              Insira o seu nome
+            </Titulo><InputContainer>
+              <InputLabel>
+                Nome
+              </InputLabel>
+              <Input
+                value={nome}
+                onChange={(event) => setNome(event.target.value)}
+                type="text" />
+            </InputContainer><InputContainer>
+              <InputLabel>
+                Saldo
+              </InputLabel>
+              <Input
+                type="number"
+                value={saldo}
+                onChange={(event) => setSaldo(event.target.value)}
+                startAdornment={<InputAdornment position="start">
+                  R$
+                </InputAdornment>} />
+            </InputContainer><Button
+              variant="contained"
+              color="primary"
+              onClick={() => history.push('/feira')}
+            >
+              Avançar
+            </Button>
+          </>
+        )
         }
-      />
-      </InputContainer>
-      <Button
-        variant="contained"
-        color="primary"
-        onClick={() => history.push('/feira')}
-      >
-        Avançar
-      </Button>
+      </UsuarioContext.Consumer>
     </Container>
   )
 };
